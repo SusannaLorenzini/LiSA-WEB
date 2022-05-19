f5fb9f7ffbec3d17a358c1c662c9b6c104e55a27
Susanna Lorenzini <lorenzini.susanna@gmail.com>
2022-05-19 11:41:42 +0200

    Updated init.php

--- init.php
+++ init.php
@@ -1,22 +1,30 @@
 <?php
+
+/**
+ * LiSA Analysis - init.php
+ * General info for LiSA Analysis
+ *
+ * @author <a href="mailto:lorenzini.susanna@gmail.com">Susanna Lorenzini</a>
+ */
+
 ini_set('error_reporting', E_ALL);
 error_reporting(E_ALL);
 
 session_start();
 
 /** STATUS CODES */
-define("NO_DATA", -1);
-define("NO_IMP_FILE", -2);
-define("ERROR_UPLOAD", -10);
-define("ERROR_INTERNAL", -500);
-define("OK", 1);
+const NO_DATA = -1;
+const NO_IMP_FILE = -2;
+const ERROR_UPLOAD = -10;
+const ERROR_INTERNAL = -500;
+const OK = 1;
 
 /** PLATFORM CONSTANT */
-define("LISACLI_JAR", "bin/lisa-cli.jar");
-define("ANALYSIS_FOLDER", "analysis");
+const LISACLI_JAR = "bin/lisa-cli.jar";
+const ANALYSIS_FOLDER = "analysis";
 
 /** GENERAL CONSTANTS */
-define("COPYRIGHT", "&copy; 2022 Susanna Lorenzini");
+const COPYRIGHT = "&copy; 2022 Susanna Lorenzini";
 
 if(!file_exists(ANALYSIS_FOLDER) && !mkdir(ANALYSIS_FOLDER))
 	die("GENERAL ERROR");
